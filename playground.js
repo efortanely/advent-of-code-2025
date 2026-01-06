f72e0f7f26d64c9cdbf00e396d19eec7961edce3
efortanely <elizabeth.fortanely@gmail.com>
2026-01-06 17:39:31 -0600

    playground pt 2

--- playground.js
+++ playground.js
@@ -8,7 +8,7 @@ function distance3D(p1, p2){
     return dx*dx + dy*dy + dz*dz;
 }
 
-function solve(points, connections){
+function solve_1(points, connections){
     const n = points.length;
     const sets = Array.from({length: n}, () => makeSet());
 
@@ -56,13 +56,100 @@ function solve(points, connections){
     return product;
 }
 
+function solve_2(points){
+    const n = points.length;
+    const sets = Array.from({length: n}, () => makeSet());
+
+    function getComponentSizes(){
+        const sizes = new Map();
+
+        for(let i = 0; i < n; i++){
+            const root = find(sets[i]);
+            sizes.set(root, (sizes.get(root) || 0) + 1);
+        }
+
+        return Array.from(sizes.values());
+    }
+
+    function getComponents(){
+        const circuits = new Map();
+
+        for(let i = 0; i < n; i++){
+            const root = find(sets[i]);
+
+            if(!circuits.has(root)){
+                circuits.set(root, []);
+            }
+
+            circuits.get(root).push(i);
+        }
+
+        return Array.from(circuits.values());
+    }
+
+    const edges = [];
+
+    for(let i = 0; i < n; i++){
+        for(let j = i + 1; j < n; j++){
+            edges.push({
+                i,
+                j,
+                dist: distance3D(points[i], points[j])
+            });
+        }
+    }
+
+    edges.sort((a, b) => {
+        if (a.dist < b.dist) return -1;
+        if (a.dist > b.dist) return 1;
+        return 0;
+    });
+
+    for(let k = 0; getComponentSizes().length > 2; k++){
+        union(sets[edges[k].i], sets[edges[k].j]);
+    }
+
+    let circuits = getComponents();
+    let minDist = Infinity;
+    let xCoord, yCoord;
+
+    for(let i = 0; i < circuits[0].length; i++){
+        for(let j = 0; j < circuits[1].length; j++){
+            let pointA = points[circuits[0][i]];
+            let pointB = points[circuits[1][j]];
+            let dist = distance3D(pointA, pointB);
+            
+            if(dist < minDist){
+                minDist = dist;
+                xCoord = pointA;
+                yCoord = pointB;
+            }
+        }
+    }
+
+    const product = xCoord.x * yCoord.x;
+    console.log('Coordinate 1:', xCoord);
+    console.log('Coordinate 2:', yCoord);
+    console.log('Product:', product.toString());
+
+    return product;
+}
+
 const lines = fs.readFileSync("input-14.txt", "utf-8")
     .split("\n")
     .filter(line => line.trim() !== "");
 
-solve(lines.map(line => {
+solve_1(lines.map(line => {
         const [x, y, z] = line.split(",").map(Number);
         return {x, y, z}
     }),
     1000
+);
+
+console.log();
+
+solve_2(lines.map(line => {
+        const [x, y, z] = line.split(",").map(Number);
+        return {x, y, z}
+    })
 );
